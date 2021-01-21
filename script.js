b96f388649b7bb3ca89ae62c713c37483daf077a
devsfe <fernando.chaves16@hotmail.com>
2021-01-21 14:37:31 -0300

    Parte 1 finalizada

---
+++ script.js
@@ -0,0 +1,20 @@
+var value1 = document.getElementById('value-1');
+var value2 = document.getElementById('value-2');
+var exchange1 = document.getElementById('exchange-1');
+var exchange2 = document.getElementById('exchange-2');
+
+
+
+
+function calculate() {
+    
+    fetch('')
+}
+
+
+
+
+value1.addEventListener('input', calculate);
+value2.addEventListener('input', calculate);
+exchange1.addEventListener('change', calculate);
+exchange2.addEventListener('change', calculate);    
