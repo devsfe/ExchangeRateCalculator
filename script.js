b5694c0291e5d7380c8bdbb2eee15211fab09842
devsfe <fernando.chaves16@hotmail.com>
2021-01-21 18:39:26 -0300

    Praticando

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-var value1 = document.getElementById('value-1');
-var value2 = document.getElementById('value-2');
-var exchange1 = document.getElementById('exchange-1');
-var exchange2 = document.getElementById('exchange-2');
+var input_1 = document.getElementById('input_1');
+var input_2 = document.getElementById('input_2');
+var exchangeRate_1 = document.getElementById('exchangeRate_1');
+var exchangeRate_2 = document.getElementById('exchangeRate_2');
 
 
 
+var valueExchange1 = exchangeRate_1.value;
+var valueExchange1 = exchangeRate_2.value;
 
 function calculate() {
+
     
-    fetch('')
+    
+     fetch(`https://api.exchangeratesapi.io/latest?base=${valueExchange1}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.rates.base * 2);
+        })
+
 }
 
-
-
-
-value1.addEventListener('input', calculate);
-value2.addEventListener('input', calculate);
-exchange1.addEventListener('change', calculate);
-exchange2.addEventListener('change', calculate);    
+// Events
+input_1.addEventListener('input', calculate);
+input_2.addEventListener('input', calculate);
+exchangeRate_1.addEventListener('change', calculate);
+exchangeRate_2.addEventListener('change', calculate);    
